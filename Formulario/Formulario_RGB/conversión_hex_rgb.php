0ce43732fd649af943952d8356d397e725e9cdb1
maestroZen <bennlli57@gmail.com>
2020-11-02 16:20:04 +0100

    Fin ejercicio

---
+++ Formulario/Formulario_RGB/conversión_hex_rgb.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hexadecimal a RGB</h1>
+    <?php
+        
+        echo "El color RGB es: ". ($_POST["color1"])."<hr>";
+        
+        $color_decimal = $_POST["color1"];
+        /*
+        echo "Prueba separacion carácteres <br>";
+        echo substr("$color_decimal",1, 2)."<br>";
+        echo substr("$color_decimal",3, 2)."<br>";
+        echo substr("$color_decimal",5, 7)."<br> <hr>";
+        */
+
+        echo "R:".hexdec(substr("$color_decimal",1, 2))."<br>";
+        echo "G:".hexdec(substr("$color_decimal",3, 2))."<br>";
+        echo "B:".hexdec(substr("$color_decimal",5, 7))."<br> <br>";
+        echo "RGB (".hexdec(substr("$color_decimal",1, 2)).",".hexdec(substr("$color_decimal",3, 2)).",".hexdec(substr("$color_decimal",5, 7)).")";
+        
+    ?>
+</body>
+</html>
--- Formulario/Formulario_RGB/conversión_rgb_hex.php
+++ Formulario/Formulario_RGB/conversión_rgb_hex.php
@@ -6,10 +6,14 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Probando</h1>
+    <h1>RGB a Hexadecimal</h1>
     <?php
         echo "R: ".$_POST["num1"].", G: ".$_POST["num2"].", B:". $_POST["num3"]."<br>";
         echo "El color Hexadecimal es: #" . dechex($_POST["num1"]).dechex($_POST["num2"]).dechex($_POST["num3"]) . "<br>";
+    ?>
+
+    <?php
+        echo "El color RGB es: ". hexdec($_POST["color1"]);
     ?>
 </body>
 </html>
