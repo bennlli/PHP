40184669b73cd7e73cfd73d48ef3c47753442ebb
bennlli <bennlli57@gmail.com>
2020-11-03 09:29:50 +0100

    03/11/2020 clase

--- Formulario/Formulario_cambio_moneda/cambioMonedaMain.php
+++ Formulario/Formulario_cambio_moneda/cambioMonedaMain.php
@@ -6,11 +6,11 @@
     <title>EUR - GBP</title>
 
 </head>
-<body>
+<body> 
     <h1>Resultado de la conversion</h1>
     <?php include 'cambioMonedaPOO.php'?>
     <?php 
-    # Volcamos la informacion del input
+    # Volcamos la informacion del input 'Euros'
     $valorEnEuros = $_POST['euros'];
     
     # Creamos el nuevo objeto
@@ -21,8 +21,25 @@
     $conversionLibra = $libra -> converEurosLibra();
 
     echo 'El valor de euros introducido es de '.$valorEnEuros.'EUR<br>';
-    echo 'El cambio a libras es de '.$libra ->converEurosLibra($valor).'GBP<hr>';
+    echo 'El cambio a Libras es de '.$libra ->converEurosLibra($valor).'GBP<hr>';
     echo $valorEnEuros.'EUR = '.$conversionLibra.'GBP';
+    ?>
+
+    <?php
+    # Volcamos la informacion del input 'Libras'
+    $valorEnLibras = $_POST['libras'];
+
+    # Creamos el nuevo objeto GBP - EUR
+    $euro = new conversion();
+    # Almacenamos el valor 'setLibras' en '$valorEUR'
+    $valorEUR = $euro -> setLibras($valorEnLibras);
+    # Almacenamos el valor de la funcion 'convertirLibrasEuros()' en '$conversionEuro'
+    $conversionEuro = $euro -> converLibrasEuros();
+
+    echo 'El valor en Libras introducido es de '.$valorEnLibras.'GBP<br>';
+    echo 'El cambio a Euros es de '.$euro ->converLibrasEuros($valorEUR).'EUR<hr>';
+    echo $valorEnLibras.'GBP = '.$conversionEuro.'EUR';
+
     ?>
     
 </body>
--- Formulario/Formulario_cambio_moneda/cambioMonedaPOO.php
+++ Formulario/Formulario_cambio_moneda/cambioMonedaPOO.php
@@ -3,20 +3,33 @@
 # Definicione de la clase
 class conversion{
     private $euros = 0;
+    private $libras = 0; 
 
     #           'GETTERS'
 public function getEuros(){
     return $this->euros;
     }
+public function getLibras(){
+    return $this->libras;
+    }
 #           'SETTERS'
 public function setEuros($euros){
     $this->euros = $euros;
     }
-# Funcion para hacer el cambio de moneda
+public function setLibras($libras){
+    $this->libras = $libras;
+    }
+# Funcion para hacer el cambio de moneda EUR - GBP
 public function converEurosLibra(){
     $valorLibra= 0.90;
     $libra=($this -> euros*$valorLibra);
     return $libra;
     }
+# Funcion para hacer el cambio de moneda GBP - EUR
+public function converLibrasEuros(){
+    $valorEuro= 1.11;
+    $euro=($this -> libras *$valorEuro );
+    return $euro;
+    }    
 }
 ?>
