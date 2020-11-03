005ab73ea4e7aeac0d775bef850bc5151a1ffc39
bennlli <bennlli57@gmail.com>
2020-11-03 13:41:59 +0100

    03/11/2020 Final

--- Formulario/Formulario_cambio_moneda/cambioMonedaMain.php
+++ Formulario/Formulario_cambio_moneda/cambioMonedaMain.php
@@ -10,9 +10,14 @@
     <h1>Resultado de la conversion</h1>
     <?php include 'cambioMonedaPOO.php'?>
     <?php 
+    # ********************************************************************************
+    #                               EUR - GBP
+    # ********************************************************************************
+    # Comprobamos si '$_POST['euros']' tiene dato y si no esta vacío
+    if (isset($_POST['euros']) && !empty($_POST['euros'])) {
+
     # Volcamos la informacion del input 'Euros'
     $valorEnEuros = $_POST['euros'];
-    
     # Creamos el nuevo objeto
     $libra = new conversion();
     # Almacenamos el valor del 'setEuros' en '$valor'
@@ -22,13 +27,20 @@
 
     echo 'El valor de euros introducido es de '.$valorEnEuros.'EUR<br>';
     echo 'El cambio a Libras es de '.$libra ->converEurosLibra($valor).'GBP<hr>';
-    echo $valorEnEuros.'EUR = '.$conversionLibra.'GBP';
+    echo $valorEnEuros.'EUR = '.$conversionLibra.'GBP <hr>';
+    
+    }
     ?>
 
     <?php
+    # ********************************************************************************
+    #                               GBP - EUR 
+    # ********************************************************************************
+    # Comprobamos si '$_POST['libras']' tiene dato y si no esta vacío
+    if (isset($_POST['libras']) && !empty($_POST['libras'])) {
+    
     # Volcamos la informacion del input 'Libras'
     $valorEnLibras = $_POST['libras'];
-
     # Creamos el nuevo objeto GBP - EUR
     $euro = new conversion();
     # Almacenamos el valor 'setLibras' en '$valorEUR'
@@ -38,9 +50,54 @@
 
     echo 'El valor en Libras introducido es de '.$valorEnLibras.'GBP<br>';
     echo 'El cambio a Euros es de '.$euro ->converLibrasEuros($valorEUR).'EUR<hr>';
-    echo $valorEnLibras.'GBP = '.$conversionEuro.'EUR';
-
+    echo $valorEnLibras.'GBP = '.$conversionEuro.'EUR<hr>';
+   
+    }
     ?>
+    <?php
+    # ********************************************************************************
+    #                               EUR - US
+    # ********************************************************************************
+    # Comprobamos si '$_POST['Dollar']' tiene dato y si no esta vacío
+    if (isset($_POST['dollar']) && !empty($_POST['dollar'])) {
     
+        # Volcamos la informacion del input 'Dollar'
+        $valorEnDollares = $_POST['dollar'];
+        # Creamos el nuevo objeto EUR - US
+        $dollar = new conversion();
+        # Almacenamos el valor 'setDollares' en '$valorEUR'
+        $valorUS = $dollar -> setDollares($valorEnDollares);
+        # Almacenamos el valor de la funcion 'converEurosUs()' en '$conversionDollar'
+        $conversionDollar = $dollar -> converEurosUs();
+    
+        echo 'El valor en Euros introducido es de '.$valorEnDollares.'EUR<br>';
+        echo 'El cambio a US es de '.$dollar ->converEurosUs($valorUS).'US<hr>';
+        echo $valorEnDollares.'EUR = '.$conversionDollar.'US<hr>';
+        
+        }
+    ?>
+        <?php 
+        # ********************************************************************************
+        #                               US - EUR
+        # ********************************************************************************
+        # Comprobamos si '$_POST['EUDollar']' tiene dato y si no esta vacío
+        if (isset($_POST['EUDollar']) && !empty($_POST['EUDollar'])) {
+
+        # Volcamos la informacion del input 'EUDollar'
+        $valorEnEurosUS = $_POST['EUDollar'];
+        # Creamos el nuevo objeto
+        $eurUsa = new conversion();
+        # Almacenamos el valor del 'setEuros' en '$valor'
+        $valorUS = $eurUsa -> setDollares($valorEnEurosUS);
+        # Almacenamos el valor de la funcion 'converUsEuro()' en '$conversionEurUs'
+        $conversionEurUs = $eurUsa -> converUsEuro();
+
+        echo 'El valor de dollares introducido es de '.$valorEnEurosUS.'US<br>';
+        echo 'El cambio a Euros es de '.$eurUsa ->converUsEuro($valorUS).'EUR<hr>';
+        echo $valorEnEurosUS.'US = '.$conversionEurUs.'EUR <hr>';
+        
+        }
+    ?>
+
 </body>
 </html>
--- Formulario/Formulario_cambio_moneda/cambioMonedaPOO.php
+++ Formulario/Formulario_cambio_moneda/cambioMonedaPOO.php
@@ -4,6 +4,7 @@
 class conversion{
     private $euros = 0;
     private $libras = 0; 
+    private $dollares =0;
 
     #           'GETTERS'
 public function getEuros(){
@@ -12,6 +13,9 @@ public function getEuros(){
 public function getLibras(){
     return $this->libras;
     }
+public function getDollares(){
+    return $this->dollares;
+    }
 #           'SETTERS'
 public function setEuros($euros){
     $this->euros = $euros;
@@ -19,6 +23,10 @@ public function setEuros($euros){
 public function setLibras($libras){
     $this->libras = $libras;
     }
+public function setDollares($dollares){
+        $this->dollares = $dollares;
+    }
+
 # Funcion para hacer el cambio de moneda EUR - GBP
 public function converEurosLibra(){
     $valorLibra= 0.90;
@@ -30,6 +38,18 @@ public function converLibrasEuros(){
     $valorEuro= 1.11;
     $euro=($this -> libras *$valorEuro );
     return $euro;
+    }    
+# Funcion para hacer el cambio de moneda EUR - US
+public function converEurosUs(){
+    $valorDollar= 1.17;
+    $dollares=($this -> dollares *$valorDollar );
+    return $dollares;
+    }
+# Funcion para hacer el cambio de moneda EUR - US
+public function converUsEuro(){
+    $valorEUR= 0.85;
+    $euros=($this -> dollares *$valorEUR );
+    return $euros;
     }    
 }
 ?>
